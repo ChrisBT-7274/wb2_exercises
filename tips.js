963c4dc9960f4468a0d9a3f86303b08b193b8c10
Christopher Cerritos <ccerritos2005@gmail.com>
2024-04-24 10:55:26 -0500

    completed tips.js

---
+++ tips.js
@@ -0,0 +1,12 @@
+//variables create as inputs for calculation
+let foodCost = 79.25;
+let tax = 6.54;
+let tip = 12.00;
+
+//do our calculations for the variables above
+let totalDue = foodCost + tax + tip;
+
+//output the results to the console 
+console.log("food cost is " + foodCost + "and tax is " + tax);
+console.log("the tip is " + tip);
+console.log("the total due is " + totalDue);
